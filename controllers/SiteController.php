36b3a5fcd9593c1fbebcef057b17deabc6a57720
Andrey and_y87 Kidin <kidin_a@list.ru>
2022-12-05 22:29:23 +0300

    1st

---
+++ controllers/SiteController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\controllers;
+
+use and_y87\nano\components\app\Controller;
+
+/**
+ *
+ */
+class SiteController extends Controller
+{
+    /**
+     * action `endex`
+     * @return void
+     */
+    public function index()
+    {
+        return $this->render();
+    }
+}
